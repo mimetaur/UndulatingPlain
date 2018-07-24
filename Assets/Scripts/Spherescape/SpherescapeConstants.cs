aef6ef0badd788c8edbd9651ac5154377dbb37e2
Nathan Koch <nathanrkoch@gmail.com>
2018-07-24 15:18:24 -0400

    clean build stripped of spherescape content

--- Assets/Scripts/Spherescape/SpherescapeConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class SpherescapeConstants
-{
-    public const float MinIntensity = 0f;
-    public const float MaxIntensity = 127.0f;
-}
